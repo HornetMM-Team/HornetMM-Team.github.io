10f610c26566661f8bd267c51c8eb7061bfd865a
Barrett <barrettbecker19@gmail.com>
2025-10-28 12:56:13 -0400

    added stuff

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("download-btn");
   btn?.addEventListener("click", () => {
-    window.open("https://github.com/HornetMM-Team/HornetMM/releases/latest", "_blank");
+    window.open("https://github.com/HornetMM-Team/HornetMM/", "_blank");
   });
 });
